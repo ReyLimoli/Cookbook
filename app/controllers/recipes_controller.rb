ee8e454968a49ccb052f2b21e19afe673d4f7d6b
Reynaldo Limoli <reynaldo.limoli@gmail.com>
2018-09-05 16:11:18 -0300

    Visitante cadastra suas receitas

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,5 +2,21 @@ class RecipesController < ApplicationController
     def show
         id = params[:id]
         @recipe = Recipe.find(id)
-    end  
+    end
+
+    def new
+    end
+
+    def create
+        # receber os dados do formulário
+        recipe_params = params.require(:recipe).permit(:title, :recipe_type,
+                                                         :cuisine, :difficulty,
+                                                         :cook_time, :ingredients,
+                                                         :cook_method) 
+        # criar a receita em si
+        recipe = Recipe.create(recipe_params)
+
+        #redirecionar para a receite recem criada
+        redirect_to recipe_path(recipe.id)
+    end
 end
